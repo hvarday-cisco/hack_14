f948188dc31627d612c21a3806a977e9fe2fed40
Harshal Varday <hvarday@cisco.com>
2020-12-02 18:05:30 +0530

    Update deregieter script

--- scripts/deregister_webhook.go
+++ scripts/deregister_webhook.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+    "fmt"
+    "hack/config"
+    "time"
+    "strconv"
+    "github.com/imroc/req"
+)
+
+func get_channel_id() string{
+    return strconv.Itoa(int(time.Now().Unix()))
+}
+
+func main(){
+
+    body := map[string]string{
+        "id": "ID",
+        "resourceId": "resourceId",
+    }
+
+    req_headers := req.Header{
+        "Authorization": conf.TOKEN,
+        "Content-Type": "application/json",
+    }
+
+    res, err := req.Post(conf.STOP_ENDPOINT, req_headers, req.BodyJSON(&body))
+
+    if err != nil{
+        fmt.Println("Error - ", err.Error())
+    } else {
+        fmt.Println(res.String())
+    }
+}
